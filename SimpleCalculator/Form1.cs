8505dbb69b712a9697abfdf84334069fe1c373fc
Rupert Duazo <rupertduazo02@gmail.com>
2020-10-18 22:40:54 -0700

    Initial Commit

---
+++ SimpleCalculator/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetAnswer delegateAnswer = new GetAnswer(Formula.getSum);
+
+
+            int num1 = Int32.Parse(textBox1.Text);
+            int num2 = Int32.Parse(textBox2.Text);
+
+            MessageBox.Show("Result:" + " " +delegateAnswer(num1,num2).ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetAnswer delegateAnswer = new GetAnswer(Formula.getDiff);
+
+
+            int num1 = Int32.Parse(textBox1.Text);
+            int num2 = Int32.Parse(textBox2.Text);
+            MessageBox.Show("Result:" + " " +delegateAnswer(num1, num2).ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GetAnswer delegateAnswer = new GetAnswer(Formula.getProduct);
+
+
+            int num1 = Int32.Parse(textBox1.Text);
+            int num2 = Int32.Parse(textBox2.Text);
+            MessageBox.Show("Result:"+" " +delegateAnswer(num1, num2).ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            GetAnswer delegateAnswer = new GetAnswer(Formula.getQuo);
+
+
+            int num1 = Int32.Parse(textBox1.Text);
+            int num2 = Int32.Parse(textBox2.Text);
+            MessageBox.Show("Result:" + " " +delegateAnswer(num1, num2).ToString());
+        }
+    }
+}
---
+++ SimpleCalculator/Formula.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public delegate int GetAnswer(int x, int y);
+
+    class Formula
+    {
+
+        public static int getSum(int num1,int num2){
+            return num1+num2;
+
+}
+        public static int getDiff(int num1,int num2)
+        {
+            return num1 - num2;
+        }
+        public static int getProduct(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static int getQuo(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+
+
+    }
+}
